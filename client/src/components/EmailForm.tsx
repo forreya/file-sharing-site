6b69ce75fd6ff312b848aedadd2a37e7e99f9be2
Ryan Lai <ryanlai12@gmail.com>
2023-04-03 08:10:39 +0100

    Fixed minor bugs + completed emailing functionality on frontend

--- client/src/components/EmailForm.tsx
+++ client/src/components/EmailForm.tsx
@@ -6,9 +6,10 @@ const EmailForm:FunctionComponent<{
 }> = ({id}) => {
   const [emailFrom, setEmailFrom] = useState("")
   const [emailTo, setEmailTo] = useState("")
-  const [message, setMessage] = useState(null)
+  const [message, setMessage] = useState("")
 
-  const handleSubmit = async () => {
+  const handleSubmit = async (event: any) => {
+    event.preventDefault()
     try {
       const {data} = await axios({
         method: "POST",
@@ -19,16 +20,16 @@ const EmailForm:FunctionComponent<{
           emailTo,
         }
       })
-      setMessage(data.response)
+      setMessage(data.message)
     } catch(error: any) {
       setMessage(error.message)
     }
   }
 
   return (
-    <div className="flex flex-col items-center justify-center w-full p-2 space-y-3" onSubmit={handleSubmit}>
+    <div className="flex flex-col items-center justify-center w-full p-2 space-y-3">
       <h3>You can also email your file.</h3>
-      <form className="flex flex-col items-center justify-center w-full p-2 space-y-3" >
+      <form className="flex flex-col items-center justify-center w-full p-2 space-y-3" onSubmit={handleSubmit}>
         <input 
           className="p-1 text-white bg-gray-800 border-2 focus:outline-none" 
           type="email" 
@@ -45,6 +46,7 @@ const EmailForm:FunctionComponent<{
         />
         <button className="bg-gray-900 p-2 my-3 rounded-md w-44 focus:outline-none" type="submit">Email</button>
       </form>
+      {message && <p className="font-medium text-green-500">{message}</p>}
     </div>
   )
 }
