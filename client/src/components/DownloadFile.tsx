89fb87c7a22b5208c05b0c5abca22596ecd63cb5
Ryan Lai <ryanlai12@gmail.com>
2023-04-03 07:03:32 +0100

    Minor changes to UI for DownloadFile component

--- client/src/components/DownloadFile.tsx
+++ client/src/components/DownloadFile.tsx
@@ -3,13 +3,13 @@ import { DownloadFileProps } from "../../libs/types";
 const DownloadFile = ({downloadPageLink}: DownloadFileProps) => {
   return (
     <div className="p-1">
-      <h1 className="my-2 text-lg font-medium">Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, animi.</h1>
-      <div className="flex space-x-3 p-2">
+      <h1 className="my-2 text-lg font-medium">Your file was successfully uploaded.</h1>
+      <div className="flex space-x-3 p-2 items-center justify-center mt-2">
         <span className="break-all">{downloadPageLink}</span>
         <img 
           src="images/copy-icon.png" 
           alt="" 
-          className="w-8 h-8 object-contain cursor-pointer" 
+          className="w-8 h-8 ml-2 object-contain cursor-pointer" 
           onClick={() => navigator.clipboard.writeText(downloadPageLink)}
         />
       </div>
